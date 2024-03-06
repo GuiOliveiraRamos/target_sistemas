df25b06a0ab1e43013cf2651b67b7291919eba35
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2024-03-06 14:53:14 -0300

    feat: exercises 1 and 2

---
+++ src/exercicio1.js
@@ -0,0 +1,17 @@
+let INDICE = 13;
+let SOMA = 0;
+let K = 0;
+
+while (K < INDICE) {
+  K = K + 1;
+  SOMA = SOMA + K;
+}
+
+console.log(SOMA);
+
+//RESPOSTA: 91
+
+// para rodar o código acima, abra um terminal na pasta src e execute o comando abaixo:
+// node exercicio1.js
+
+
---
+++ src/exercicio2.js
@@ -0,0 +1,32 @@
+function fibonacciSequence(n) {
+    let fibonacci = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+    }
+
+    return fibonacci;
+}
+
+function isInFibonacciSequence(number, sequence) {
+    return sequence.includes(number);
+}
+
+const numberToCheck = parseInt(process.argv[2]);
+
+if (isNaN(numberToCheck)) {
+    console.log('Por favor, insira um número válido.');
+    process.exit();
+}
+
+const fibonacci = fibonacciSequence(numberToCheck);
+
+if (isInFibonacciSequence(numberToCheck, fibonacci)) {
+    console.log(`${numberToCheck} pertence à sequência de Fibonacci.`);
+} else {
+    console.log(`${numberToCheck} não pertence à sequência de Fibonacci.`);
+}
+
+// para rodar o código acima, abra um terminal na pasta src e execute o comando abaixo:
+// node exercicio2.js + <número>
+// Exemplo: node exercicio2.js 13
